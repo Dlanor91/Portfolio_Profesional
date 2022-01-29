a5376ca4525f2d011491ad4bee866031107877ac
Ronald Lima Nunez <limaronald926@gmail.com>
2022-01-29 14:49:28 -0300

    Creating Category Model

---
+++ Shared/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models
+{
+    internal class Category
+    {
+        [Key] /*esto define que es la clave de mi sistema de BD*/
+        public int CategoryID { get; set; } 
+
+        [Required] /*significa que es un campo requerido de mi BD not null y su tamanno respecto a la BD*/
+        [MaxLength(256)]
+        public string ThumbnailImagePath { get; set; }
+        [Required]
+        [MaxLength(128)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(1024)]
+        public string Description { get; set; }
+
+    }
+}
