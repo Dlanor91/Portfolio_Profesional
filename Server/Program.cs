b173e5803cc1848cca3c8de588fae54265b64ae4
Ronald <rl8506@gmail.com>
2023-01-03 21:39:51 -0300

    using the DB COntext(>net 6Update)

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,25 +1,45 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Models;
+using Server.Data;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args); //args son argumnentos
+
+builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlite(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    )); ;
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Server", Version = "v1" });
+});
+builder.Services.AddRouting(options=> options.LowercaseUrls = true);
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Server v1"));
 }
 
 app.UseHttpsRedirection();
 
+app.UseStaticFiles();   
+
+app.UseRouting();
+
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
