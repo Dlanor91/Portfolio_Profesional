68cf7a9955e1e444338f3223fe8ae447c070a49d
Ronald <rl8506@gmail.com>
2023-01-05 20:54:41 -0300

    creating Api

---
+++ Server/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using Shared.Models;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //para usar como API
+    public class CategoriesController : ControllerBase
+    {
+        private readonly AppDBContext _appDBContext;
+
+        //localhost:5001/api/categories
+        public CategoriesController(AppDBContext appDBContext)
+        {
+            _appDBContext = appDBContext; 
+        }
+
+        //ourwebsita.com/api/categories
+        [HttpGet]
+        public async Task<IActionResult> Get() 
+        {
+            List<Category> categories = await _appDBContext.Categories.ToListAsync();
+            return Ok(categories);
+        }
+
+    }
+}
